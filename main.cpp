47db37b8f0eb192fe653950a7f6e3448f6dd504b
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-12-07 22:02:20 +0200

    open empty window

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+static const struct {
+    float x, y;
+    float r, g, b;
+} vertices[3] = {{-0.6f, -0.4f, 1.f, 0.f, 0.f},
+                 {0.6f, -0.4f, 0.f, 1.f, 0.f},
+                 {0.f, 0.6f, 0.f, 0.f, 1.f}};
+
+static void errorCallback(int error, const char* description) {
+    std::cerr << "Error: " << description << '\n';
+}
+
+static void
+keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization failed.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    GLFWwindow* window =
+            glfwCreateWindow(640, 480, "Capstone", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Window creation failed.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW initialization failed.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
