de8a45bf14599e86c42819ff0dc4fee0398730f2
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-12-11 15:13:20 +0200

    fix Camera

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -2,12 +2,30 @@
 
 #include "Transformable.hpp"
 
-class Camera : public Transformable {
+class Camera {
 private:
+    glm::vec3 m_position;
+    glm::vec3 m_rotation;
+    glm::vec3 m_direction;
+
     glm::mat4 m_view;
+    glm::mat4 m_perspective;
 
 public:
-    Camera(glm::vec3 position, glm::vec3 direction);
+    Camera() = default;
+    Camera(glm::vec3 position,
+           glm::vec3 direction,
+           float fov,
+           float aspect,
+           float near,
+           float far);
 
     glm::mat4 getView();
+    glm::mat4 getPerspective();
+
+    void setView(glm::vec3 position, glm::vec3 direction);
+    void setPerspective(float fov, float aspect, float near, float far);
+
+private:
+    glm::mat4 createView();
 };
