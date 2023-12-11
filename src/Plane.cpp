d3aeb4452368584353c7ab1fac4fa8861c5ce4d1
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-12-11 15:14:12 +0200

    fix Plane

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -9,18 +9,20 @@ Plane::Plane(uint32 width, uint32 height) {
 uint32 Plane::indexFrom2D(int x, int y) { return y * m_width + x; }
 
 void Plane::generate(uint32 width, uint32 height) {
-    std::vector<float> vertices;
+    m_width = width;
+    m_height = height;
+
     std::vector<uint32> indices;
+    std::vector<Vertex> vertices;
     for (int y = 0; y <= m_height; y++) {
         for (int x = 0; x <= m_width; x++) {
-            vertices.push_back(x * 0.5f);
-            vertices.push_back(y * 0.5f);
-            vertices.push_back(0.0f);
+            Vertex vertex{x * 0.5f, y * 0.5f, 0.0f};
+            vertices.push_back(vertex);
         }
     }
 
-    for (int y = 0; y < m_height; y++) {
-        for (int x = 0; x < m_width; x++) {
+    for (int y = 0; y <= m_height; y++) {
+        for (int x = 0; x <= m_width; x++) {
             indices.push_back(indexFrom2D(x, y));
             indices.push_back(indexFrom2D(x + 1, y));
             indices.push_back(indexFrom2D(x + 1, y + 1));
@@ -30,4 +32,6 @@ void Plane::generate(uint32 width, uint32 height) {
             indices.push_back(indexFrom2D(x, y + 1));
         }
     }
+
+    setData(indices, vertices);
 }
